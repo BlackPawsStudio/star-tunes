af528e7cba2d09d77e56dd4d20d5698f64221abb
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-30 14:38:41 +0300

    try

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
   header('Content-Type: application/json');
-  header("Access-Control-Allow-Origin: *");  
-  echo [ "b" => "aboba" ];
+  header("Access-Control-Allow-Origin: * ");  
+  echo [ "b" => "boba" ];
 ?>
