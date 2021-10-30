a50d0ab80b60d4e123a0277489e2b4b878d9579c
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-30 14:16:10 +0300

    try

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-  header('Content-Type: application/json');
-  header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  // header('Content-Type: application/json');
   header("Access-Control-Allow-Origin: *");  
   echo 'aboba';
 ?>
