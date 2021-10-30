bf510ff3a9bbae2060589ae7a8c95ccb5bb26bff
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-30 19:34:10 +0300

    try to output data

--- index.php
+++ index.php
@@ -10,17 +10,28 @@
   $query_builder = TRUE;
   // Connect to DB
   $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
   if ($conn -> connect_error) {
     echo json_encode('died from cringe');
     die("Connection failed: " . $conn -> connect_error);
   }
   
-  $sql = "SELECT * FROM colordata";
+  // $sql = "SELECT * FROM colordata";
 
-  $result = $conn->query($sql);
+  // $result = $conn->query($sql);
   
-  echo json_encode($result);
+  // if ($result -> num_rows > 0) {
+  //   // output data of each row
+  //   echo json_encode();
+  // } else {
+  //   echo "0 results";
+  // }
+
+  $sql = "SELECT * FROM colordata";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    echo $user;
+
   $conn->close();
 
   // echo json_encode('Привет, ' . htmlspecialchars($_GET["name"]) . '!');
