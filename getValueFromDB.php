6af0c24edd6f4e59f72882864984d628468a4b34
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-30 12:15:44 +0300

    test server

--- getValueFromDB.php
+++ getValueFromDB.php
@@ -1,3 +1,6 @@
-<?php  
+<?php
+  header('Content-Type: application/json');
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  header("Access-Control-Allow-Origin: *");  
   echo 'aboba';
 ?>
