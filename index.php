77e526d4d42eea12d1e0a8f11dfca694f7abc920
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-30 18:34:34 +0300

    check sql connection

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
   header("Access-Control-Allow-Origin: * ");  
-  echo json_encode('Привет, ' . htmlspecialchars($_GET["name"]) . '!');
+  
+  $servername = '@eu-cdbr-west-01.cleardb.com';
+  $username = 'b3c3aff8b732c2';
+  $password = '732bc427';
+  $dbname = 'heroku_01d49df919a24e7';
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error);
+  }
+  
+  $sql = "SELECT * FROM colordata";
+
+  $result = $conn->query($sql);
+  
+  if ($result -> num_rows > 0) {
+    // output data of each row
+    echo json_encode($sql);
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+
+  // echo json_encode('Привет, ' . htmlspecialchars($_GET["name"]) . '!');
 ?>
