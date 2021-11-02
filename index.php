1e47abc9c2df064e5a1883766e4e9886dec17c7f
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-11-02 12:34:25 +0300

    add create user function

--- index.php
+++ index.php
@@ -52,7 +52,8 @@
   switch (htmlspecialchars($_POST['create'])) {
     case 'user':
       $sql = "INSERT INTO `users` (`id`, `name`, `password`, `rating`, `marked`, `pfp`) VALUES (".htmlspecialchars($_POST['id']).",".htmlspecialchars($_POST['name'])." ,".htmlspecialchars($_POST['password']).",".htmlspecialchars($_POST['rating']).",".htmlspecialchars($_POST['marked']).", ".htmlspecialchars($_POST['pfp']).")";
-      $result = mysqli_query($conn, $sql);
+      // $result = mysqli_query($conn, $sql);
+      $conn->query($sql);
       break;
   }
 
